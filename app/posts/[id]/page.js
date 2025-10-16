25ae07fbbe843dbbf51e068d0799f4f32679e2b4
AsifAhmedTajid <asifahmedtanjid@gmail.com>
2025-10-16 17:15:07 +0600

    dynamic metadata

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,5 +1,13 @@
 import getPost from "@/lib/getPost";
 import React from "react";
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const post = await getPost(id);
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
 
 export default async function postPage({ params }) {
   const { id } = await params;
